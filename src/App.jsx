acbca706350d7382803add56efba111ec9a04c43
Sumit031202 <sumitkr.031202@gmail.com>
2026-01-29 14:44:18 +0530

    initial commit: vite + tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-600">
+      <h1 className="text-4xl font-bold text-white underline">
+        Tailwind is Working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
